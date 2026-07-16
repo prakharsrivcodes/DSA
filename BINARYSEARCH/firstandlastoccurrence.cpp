2e639201cc5296d1bcf7c0134de38cde848eb6b4
Prakhar Srivastava <prakharsriv.codes@gmail.com>
2026-07-16 13:26:55 +0530

    "First and last occurrence of an element in a sorted array"

--- BINARYSEARCH/firstandlastoccurrence.cpp
+++ BINARYSEARCH/firstandlastoccurrence.cpp
@@ -76,3 +76,77 @@ int main()
 
     cout << ans.first << " " << ans.second;
 }
+
+// normal binary search
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstOcc(vector<int> &arr, int target)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+    int first = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+        {
+            first = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return first;
+}
+
+int lastOcc(vector<int> &arr, int target)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+    int last = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+        {
+            last = mid;
+            low = mid + 1;
+        }
+        else if (arr[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return last;
+}
+
+int main()
+{
+    vector<int> arr = {2, 4, 6, 8, 8, 8, 11, 13};
+
+    int target;
+    cin >> target;
+
+    cout << "First Occurrence : " << firstOcc(arr, target) << endl;
+    cout << "Last Occurrence  : " << lastOcc(arr, target) << endl;
+
+    return 0;
+}
