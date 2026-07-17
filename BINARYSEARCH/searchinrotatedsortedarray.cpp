d36b52d307da3bc705cdacc45ab698b37140e1df
Prakhar Srivastava <prakharsriv.codes@gmail.com>
2026-07-17 11:47:19 +0530

    "added solution for search in rotated sorted array"

--- BINARYSEARCH/searchinrotatedsortedarray.cpp
+++ BINARYSEARCH/searchinrotatedsortedarray.cpp
@@ -55,3 +55,72 @@ int main()
 
     return 0;
 }
+// for duplicates
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool search(vector<int>& arr, int n, int k)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while(low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if(arr[mid] == k)
+            return true;
+
+        // Can't identify the sorted half
+        if(arr[low] == arr[mid] && arr[mid] == arr[high])
+        {
+            low++;
+            high--;
+            continue;
+        }
+
+        // Left half is sorted
+        if(arr[low] <= arr[mid])
+        {
+            if(arr[low] <= k && k <= arr[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        // Right half is sorted
+        else
+        {
+            if(arr[mid] <= k && k <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {2,5,6,0,0,1,2};
+
+    int target;
+    cin >> target;
+
+    if(search(arr, arr.size(), target))
+        cout << "Found";
+    else
+        cout << "Not Found";
+
+    return 0;
+}
